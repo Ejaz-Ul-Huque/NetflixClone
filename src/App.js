2929b94cb342055bea2a4c9a212e8b9a155348bd
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2022-12-29 23:28:50 +0530

    Banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Homescreen from './Screen/Homescreen';
 
 function App() {
   return (
     <div className="App">
-     <h1 className='text-2xl'>Hi</h1>
+     <Homescreen />
     </div>
   );
 }
---
+++ src/Screen/Homescreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../Components/Banner'
+
+function Homescreen() {
+  return (
+    <div>
+        <Banner />
+    </div>
+  )
+}
+
+export default Homescreen
