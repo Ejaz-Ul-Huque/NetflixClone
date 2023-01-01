96fc3cfd521387103f12fd8093bae88e1cc3899c
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-01-01 11:41:06 +0530

    Banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -6,7 +6,10 @@ function Banner() {
         <div className='h-[450px] relative text-white object-contain max-w-[100rem] mx-auto'>
         <div className='text-left pt-[140px] h-[150px]'>
             <h1 className='text-6xl pb-2 font-black font-serif'>Stranger Things</h1>
-            <p>When a young boy disappears, his mother, a police chief and his friends must confront terrifying supernatural forces in order to get him back.</p>
+            <p>When a young boy disappears, his mother, <br/>
+             a police chief and his friends must confront <br/>
+              terrifying supernatural forces in order <br/>
+               to get him back.</p>
             <div className='flex space-x-3 mt-4'>
                 <button className='bg-white text-gray-700 px-6 py-1 font-bold'>Play</button>
                 <button className='bg-gray-800 px-6 py-1 font-bold'>My list</button>
