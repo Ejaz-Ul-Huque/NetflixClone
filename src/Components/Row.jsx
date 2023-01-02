19ce00b1da9aac0c0ed08ddad6a9e9421f312387
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-01-02 21:23:04 +0530

    Row

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
-
   const base_url = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {}, [fetchUrl]);
