602c2c739aacbf900b899b919e886e553f7b7063
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-01-02 21:50:43 +0530

    base_url and useEffect

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
+import axios from "../axios"
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
   const base_url = "https://image.tmdb.org/t/p/original/";
 
-  useEffect(() => {}, [fetchUrl]);
+  useEffect(() => {
+    async function fetchData(){
+      const result = await axios.get(fetchUrl) ;
+      console.log(result)
+    
+    }
+
+    fetchData() ;
+  }, [fetchUrl]);
 
   return (
     <div className=" bg-black">
