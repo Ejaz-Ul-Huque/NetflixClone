23a358a7ca01984a0874b34c5038905c21451838
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-01-05 01:50:53 +0530

    API and banner

--- src/Screen/Homescreen.jsx
+++ src/Screen/Homescreen.jsx
@@ -3,21 +3,22 @@ import { useEffect } from 'react';
 import Banner from '../Components/Banner';
 import Navbar from '../Components/Navbar';
 import Row from '../Components/Row';
-import axios from 'axios';
+// import axios from 'axios';
+import requests from '../Requests';
 
 function Homescreen() {
 
-  useEffect(()=>{
-    axios.get('https://api.themoviedb.org/3/movie/550?api_key=e8ab91b21b368fbc8e4eca430e357342')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  },[])
+  // useEffect(()=>{
+  //   axios.get('https://api.themoviedb.org/3/movie/550?api_key=e8ab91b21b368fbc8e4eca430e357342')
+  // .then(function (response) {
+  //   // handle success
+  //   console.log(response);
+  // })
+  // .catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // })
+  // },[])
   
 
   return (
@@ -25,15 +26,14 @@ function Homescreen() {
         <Navbar />
         <Banner />
         <div className=' max-w-[100rem] mx-auto px-6 py-12'>
-        <Row title={"Netflix Orignals"}  />
-        <Row title={"Trending"}  />
-        <Row title={"Top Rated"}  />
-        <Row title={"Action"}  />
-        <Row title={"Romance"}  />
-        <Row title={"Comedy"}  />
-        <Row title={"Documentries"}  />
-        <Row title={"Horror"}  />
-        <Row title={"Sci-Fi"}  />
+        <Row title={"Netflix Orignals"} fetchUrl={requests.fetchNetflixOriginals}   />
+        <Row title={"Trending"} fetchUrl={requests.fetchTrending}  />
+        <Row title={"Top Rated"} fetchUrl={requests.fetchTopRated} />
+        <Row title={"Action"} fetchUrl={requests.fetchActionMovies} />
+        <Row title={"Romance"} fetchUrl={requests.fetchRomanceMovies} />
+        <Row title={"Comedy"} fetchUrl={requests.fetchComedyMovies} />
+        <Row title={"Documentries"} fetchUrl={requests.fetchDocumentaries} />
+        <Row title={"Horror"} fetchUrl={requests.fetchHorrorMovies} />
         </div>
 
     </div>
