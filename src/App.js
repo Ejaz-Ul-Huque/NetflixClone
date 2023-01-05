2d48ed0a866fb6172bae7ea8cffb7e9d6b5bef2c
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-01-06 00:13:59 +0530

    firebase done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Homescreen from "./Screen/Homescreen";
+import LoginScreen from "./Screen/LoginScreen";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import app from './firebase'
 
-import './App.css';
-import Homescreen from './Screen/Homescreen';
-import LoginScreen from './Screen/LoginScreen';
 
 function App() {
-  const user = true ;
+  const auth = getAuth(app);
+
+
+
+  const schema = {email : "" , pass : ""} ;
+  const [ cred  ,setCred ] = useState(schema) ;
+  const [user ,setUser] = useState(true) ;
+
+
+  const createUser = async () => {
+    await createUserWithEmailAndPassword(auth, cred.email, cred.pass)
+    .then(userCredential => {
+      const u = userCredential.user;
+      setUser(u)
+    })
+    .catch(error => {
+      setUser(null)
+    });
+  }
+
+
+
+  const loginUser = async () => {
+    await signInWithEmailAndPassword(auth, cred.email, cred.pass)
+    .then(userCredential => {
+      const u = userCredential.user;
+      setUser(u)
+    })
+    .catch(error => {
+      setUser(null)
+    });
+  }
+
+  
   return (
-    <div className="">
-      {
-          !user ? <LoginScreen /> : <Homescreen />
-        }
+    <div className="App bg-black">
+        <Routes>
+          <Route path="/" element={ user ? <Homescreen /> : <LoginScreen Cred={setCred} /> } />
+          <Route path="/login" element={<LoginScreen Cred={setCred} />} />
+        </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// import { redirect } from "react-router-dom";
+
+  // const loader = async () => {
+  //   const user = await getUser();
+  //   if (!user) {
+  //     return redirect("/login");
+  //   }
+  // };
